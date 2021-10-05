0b245535918e1208ced50ac2adec07b5a8a4debd
Saathvika Ajith <saathvika.ajith@gmail.com>
2021-10-04 18:04:27 -0700

    hey besties - starting hack

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
-from images import image_data, draw_text_in_image,rgb_saathvika
+from images import image_data, draw_text_in_image
 
 # create a Flask instance
 app = Flask(__name__)
@@ -24,8 +24,13 @@ def rgbsarayu():
 
 @app.route('/rgbsaathvika/')
 def rgbsaathvika():
-    rgb_saathvika()
     return render_template("rgb_saathvika.html", images=image_data())
+
+
+@app.route('/rgbpranavi/')
+def rgbpranavi():
+    draw_text_in_image("Team Color's Text")
+    return render_template("rgb_pranavi.html", images=image_data())
 
 
 @app.route('/home/')
@@ -33,10 +38,20 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/rgbpranavi/')
-def rgbpranavi():
-    draw_text_in_image("Team Color's Text")
-    return render_template("rgb_pranavi.html", images=image_data())
+@app.route('/greet/')
+def greet():
+    return render_template("greet.html")
+
+
+@app.route('/greet/', methods=['GET', 'POST'])
+def greet2():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", name=name)
+    # starting and empty input default
+    return render_template("greet.html", name="World")
 
 
 @app.route('/sarayu/')
@@ -87,20 +102,39 @@ def pranavi2():
     return render_template("pranavi.html", name="World")
 
 
-@app.route('/greet/')
-def greet():
-    return render_template("greet.html")
+@app.route('/logicgates/')
+def logicgates():
+    return render_template("logic_gates.html")
 
 
-@app.route('/greet/', methods=['GET', 'POST'])
-def greet2():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
-    # starting and empty input default
-    return render_template("greet.html", name="World")
+@app.route('/red/')
+def red():
+    return render_template("red.html")
+
+
+@app.route('/orange/')
+def orange():
+    return render_template("orange.html")
+
+
+@app.route('/yellow/')
+def yellow():
+    return render_template("yellow.html")
+
+
+@app.route('/green/')
+def green():
+    return render_template("green.html")
+
+
+@app.route('/blue/')
+def blue():
+    return render_template("blue.html")
+
+
+@app.route('/purple/')
+def purple():
+    return render_template("purple.html")
 
 
 # runs the application on the development server
