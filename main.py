eb79b6f15e9b2bc817cd418c611c8061cff7eada
kamryns <kamrynsinsuan@gmail.com>
2022-01-06 22:49:57 -0800

    save notes to reminders page, added background music to about page

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # import "packages" from flask
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import requests
+import os
+import pickle
+import math
+import random
 
 
 import json
+
+nextAnswerString=""
+
 # create a Flask instance
 app = Flask(__name__)
 
@@ -28,12 +35,57 @@ def walruses():
 @app.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
 @app.route('/music/')
 def music():
     return render_template("music.html")
-@app.route('/reminders/')
+
+@app.route('/reminders/', methods=['GET'])
 def reminders():
-    return render_template("reminders.html")
+    #kdf = open('/tmp/KammyDebug.txt', 'wt+')
+    MAX_NUM_NOTES=8
+    fileInfoReminder = [0 for i in range(MAX_NUM_NOTES)]
+    attribNames = [0 for i in range(MAX_NUM_NOTES)]
+    tmpNotes = ['' for i in range(MAX_NUM_NOTES)]
+    #kdf.writelines('in reminders: \n')
+    for ii in range(MAX_NUM_NOTES):
+        # create file if it doesnt exist.
+        try:
+            fileInfoReminder[ii] = open('/tmp/fileInfo%d.bin'%ii, 'rb')
+        except:
+            os.system('touch /tmp/fileInfo%d.bin'%ii)
+            fileInfoReminder[ii] = open('/tmp/fileInfo%d.bin'%ii, 'rb')
+
+        try:
+            dataDict = pickle.load(fileInfoReminder[ii])
+            #kdf.writelines('Reading Notes: %s\n' % dataDict)
+            tmpNotes[ii] = dataDict['tmpNote']
+        except:
+            tmpNotes[ii] = ""
+            #kdf.writelines('Error Deleting All Notes')
+        #<textarea id="box7" style="margin: 2%" rows="9" cols="32" placeholder="remind me..."></textarea>
+        attribNames[ii] = {'idName': "box%d"%(ii+1), 'style': "margin: 50%", 'rows': "9", 'cols':"32", "placeholder":"remind me..."}
+
+    #kdf.close()
+    return render_template("reminders.html", attrib_names=attribNames, persistentNotes=tmpNotes, numUpdates=MAX_NUM_NOTES)
+
+
+@app.route('/reminders/', methods=['POST'])
+def storeNotes():
+    #kdf = open('/tmp/KammyDebug.txt', 'wt+')
+    blob = request.data
+    dataDict = json.loads(bytes.decode(blob))
+    #kdf.writelines('Storing Notes: %s\n' % dataDict)
+    boxIdx = int(dataDict['idName'][3:])-1
+    fileInfoStorage = open('/tmp/fileInfo%d.bin'%boxIdx, 'wb+')
+    pickle.dump(dataDict, fileInfoStorage)
+    #kdf.writelines('Storing Notes: %s\n' % dataDict)
+    #kdf.close()
+    fileInfoStorage.close()
+    attribNames=[]
+    return render_template("reminders.html", attrib_names=attribNames, numUpdates=0)
+
+
 @app.route('/hwtimer/')
 def hwtimer():
     return render_template("hwtimer.html")
@@ -42,9 +94,39 @@ def hwtimer():
 def stub():
     return render_template("stub.html")
 
-@app.route('/kamryn_abt/')
+@app.route('/kamryn_abt/', methods=['GET'])
 def kamryn_abt():
-    return render_template("kamryn_abt.html", stats=response.json())
+    #if request.form:
+        #name = request.form.get("name")
+        #if len(name) != 0:
+            #return render_template("kamryn_abt.html", name=name)
+    #, methods=['GET']
+    global nextAnswerString
+    answerString = nextAnswerString
+
+    arrayOfAnswers = ['Yes, definitely!',
+                      'No, absolutely not.',
+                      'Maybe...',
+                      'Concentrate and ask',
+                      'Unlikely',
+                      'Certainly',
+                      'Do not count on it',
+                      'As I see it, yes',
+                      'I am not sure',
+                      'Most likely',
+                      'You may rely on it']
+
+    randIdx = int(math.floor(random.random()*len(arrayOfAnswers)))
+    nextAnswerString = arrayOfAnswers[randIdx]
+    return render_template("kamryn_abt.html", currentAnswer=answerString)
+
+#@app.route('/kamryn_abt/', methods=['GET', 'POST'])
+#def kamryn_abt():
+    #if request.form:
+        #name = request.form.get("name")
+        #if len(name) != 0:
+            #return render_template("kamryn_abt.html", name=name)
+    #return render_template("kamryn_abt.html", name="World")
 
 @app.route('/riya_abt/')
 def riya_abt():
@@ -133,6 +215,7 @@ def aboutus():
 @app.route('/wheel/')
 def wheel():
     return render_template("wheel.html")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
