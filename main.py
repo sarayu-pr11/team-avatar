9026f799dfdb8f7b12188dd3ee84ed5307a0af61
kamryns <kamrynsinsuan@gmail.com>
2022-01-28 13:16:24 -0800

    fixed reminders blueprint

--- main.py
+++ main.py
@@ -144,54 +144,6 @@ def aboutus():
 def calculator():
     return render_template("calculator.html")
 
-
-@app.route('/reminders/', methods=['GET'])
-def reminders():
-    #kdf = open('/tmp/KammyDebug.txt', 'wt+')
-    MAX_NUM_NOTES=8
-    fileInfoReminder = [0 for i in range(MAX_NUM_NOTES)]
-    attribNames = [0 for i in range(MAX_NUM_NOTES)]
-    tmpNotes = ['' for i in range(MAX_NUM_NOTES)]
-    #kdf.writelines('in reminders: \n')
-    for ii in range(MAX_NUM_NOTES):
-        # create file if it doesnt exist.
-        try:
-            fileInfoReminder[ii] = open('/tmp/fileInfo%d.bin'%ii, 'rb')
-        except:
-            os.system('touch /tmp/fileInfo%d.bin'%ii)
-            fileInfoReminder[ii] = open('/tmp/fileInfo%d.bin'%ii, 'rb')
-
-        try:
-            dataDict = pickle.load(fileInfoReminder[ii])
-            #kdf.writelines('Reading Notes: %s\n' % dataDict)
-            tmpNotes[ii] = dataDict['tmpNote']
-        except:
-            tmpNotes[ii] = ""
-            #kdf.writelines('Error Deleting All Notes')
-        #<textarea id="box7" style="margin: 2%" rows="9" cols="32" placeholder="remind me..."></textarea>
-        attribNames[ii] = {'idName': "box%d"%(ii+1), 'style': "margin: 50%", 'rows': "9", 'cols':"32", "placeholder":"remind me..."}
-
-    #kdf.close()
-    return render_template("reminders.html", attrib_names=attribNames, persistentNotes=tmpNotes, numUpdates=MAX_NUM_NOTES)
-
-
-@app.route('/reminders/', methods=['POST'])
-def storeNotes():
-    #kdf = open('/tmp/KammyDebug.txt', 'wt+')
-    blob = request.data
-    dataDict = json.loads(bytes.decode(blob))
-    #kdf.writelines('Storing Notes: %s\n' % dataDict)
-    boxIdx = int(dataDict['idName'][3:])-1
-    fileInfoStorage = open('/tmp/fileInfo%d.bin'%boxIdx, 'wb+')
-    pickle.dump(dataDict, fileInfoStorage)
-    #kdf.writelines('Storing Notes: %s\n' % dataDict)
-    #kdf.close()
-    fileInfoStorage.close()
-    attribNames=[]
-    return render_template("reminders.html", attrib_names=attribNames, numUpdates=0)
-
-
-
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
