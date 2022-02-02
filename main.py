b87079235d444f09228b06aaf880dfad7b19b4ee
nataliebeckwith <natalieb60251@stu.powayusd.com>
2022-02-02 12:43:01 -0800

    made 404 page

--- main.py
+++ main.py
@@ -144,6 +144,11 @@ def aboutus():
 def calculator():
     return render_template("calculator.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
