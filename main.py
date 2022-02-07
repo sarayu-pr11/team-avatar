1d2fc8781e0d7448022ccc6456498986b9dfc28c
briantang <brian.g.tang.135@gmail.com>
2022-02-06 22:06:36 -0800

    dictionary

--- main.py
+++ main.py
@@ -155,6 +155,26 @@ def calculator():
 def tictactoe():
     return render_template("tictactoe.html")
 
+@app.route('/dictionary/', methods=['GET','POST'])
+def dictionary():
+    try:
+        keyword = request.form['keyword']
+    except:
+        keyword = "study"
+    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
+    querystring = {"entry":keyword}
+    headers = {
+        'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
+        'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code<400:
+        results = json.loads(response.content.decode("utf-8"))
+        return render_template("dictionary.html", results=results, word=keyword)
+    else:
+        return render_template("dictionary.html", word=keyword)
+    # print(response.text)
+
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
